9cbe9c0d78041b96aa4b335af91cc60c798c7508
Sukanta Naskar <sukanta.naskar@gmail.com>
2025-09-10 09:46:40 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,16 @@
+# Configure the AWS Provider
+terraform {
+  required_version = "~> 1.12.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Specify the AWS region
+provider "aws" {
+  region = var.aws_region
+}
